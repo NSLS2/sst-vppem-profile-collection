41a0989608143300202b268c36fa8b3220059927
Charles Titus <ctitus@bnl.gov>
2026-04-14 10:19:09 -0400

    VPPEM working in sim

--- scripts/gui-dev.sh
+++ scripts/gui-dev.sh
@@ -2,7 +2,8 @@
 set -e
 set -o xtrace
 pip install -e /home/xf07id1/collection_packages/nbs-core
-pip install -e /home/xf07id1/collection_packages/sst_base
+pip install -e /home/xf07id1/collection_packages/sst-base
 pip install -e /home/xf07id1/collection_packages/nbs-gui
+pip install -e /home/xf07id1/collection_packages/livetable
 $(dirname "$0")/gui-start.sh
 
--- scripts/qs-dev.sh
+++ scripts/qs-dev.sh
@@ -3,6 +3,6 @@ set -e
 set -o xtrace
 pip install -e /home/xf07id1/collection_packages/nbs-core
 pip install -e /home/xf07id1/collection_packages/nbs-bl
-pip install -e /home/xf07id1/collection_packages/sst_base
+pip install -e /home/xf07id1/collection_packages/sst-base
 pip install -e /home/xf07id1/collection_packages/vppem
 $(dirname "$0")/qs-start.sh
