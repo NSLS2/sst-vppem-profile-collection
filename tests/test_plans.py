910ec6f24f88e8efbc233cc2a8ea237ef2826ab3
Charles Titus <ctitus@bnl.gov>
2026-04-24 17:04:00 -0400

    Modifying tests to have some more output

--- tests/test_plans.py
+++ tests/test_plans.py
@@ -2,12 +2,15 @@ import pytest
 from nbs_bl.plans.scans import nbs_count, nbs_energy_scan
 
 def test_nbs_count(RE, db):
-    RE(nbs_count(10))
+    print("Running nbs_count test...")
+    RE(nbs_count(10, dwell=1.0))
+    print("nbs_count test completed")
     run = db[-1]
     assert run.start['plan_name'] == "nbs_count"
     assert run.primary['data']['time'].shape == (10,)
 
 def test_nbs_energy_scan(RE, db):
-    RE(nbs_energy_scan(1800, 1.0, 1805, dwell=1.0))
+    print("Running nbs_energy_scan test...")
+    RE(nbs_energy_scan(700, 1.0, 705, dwell=1.0))
     run = db[-1]
     assert run.primary['data']['time'].shape == (6,)
