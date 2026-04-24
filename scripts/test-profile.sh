bb0693f5d7300d661d835621487c6492d4b46392
Charles Titus <ctitus@bnl.gov>
2026-04-24 16:19:26 -0400

    Installing vppem with no deps

--- scripts/test-profile.sh
+++ scripts/test-profile.sh
@@ -7,6 +7,7 @@ PROFILE_ROOT="$(cd -- "${SCRIPT_DIR}/.." && pwd)"
 TESTS_DIR="${PROFILE_ROOT}/tests"
 
 export TESTS_DIR
-pip install -e /home/xf07id1/collection_packages/vppem
+pip install -v -e /home/xf07id1/collection_packages/vppem --no-deps
+echo "test-profile: pip install -e vppem (--no-deps) completed"
 python -c "import importlib.util as u; print(u.find_spec('vppem'))"
 ipython --profile collection -c 'import os, sys, pytest, time; time.sleep(10); print(os.environ["TESTS_DIR"]); rc = pytest.main(["-v", os.environ["TESTS_DIR"]]); sys.stdout.flush(); sys.stderr.flush(); os._exit(rc)'
