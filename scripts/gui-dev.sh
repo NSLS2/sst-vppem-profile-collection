5b7f2dc33b795fbfc5408b2550a2f7ab7720d76a
Charles Titus <ctitus@bnl.gov>
2026-05-27 13:53:12 -0400

    Updates to dependencies for better display of RBDs

--- scripts/gui-dev.sh
+++ scripts/gui-dev.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 set -e
 set -o xtrace
-pip install -e /home/xf07id1/collection_packages/nbs-core
 pip install -e /home/xf07id1/collection_packages/nbs-bl
 pip install -e /home/xf07id1/collection_packages/sst-base
 pip install -e /home/xf07id1/collection_packages/nbs-gui
